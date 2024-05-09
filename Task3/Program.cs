160b32ec52050eae565085b2680f32d62a581df3
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-05-09 10:45:58 +0500

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(array[index]);
+            PrintArrayReversed(array, index - 1);
+        }
+    }
+}
