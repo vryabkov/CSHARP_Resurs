1bbf6595fdac6105bb3906bb3a29bbc901d2c40c
Ryabkov Vitaliy <ryabkof@gmail.com>
2024-05-09 10:29:23 +0500

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа от {M} до {N} включительно:");
+
+        PrintNumbersRecursive(M, N);
+    }
+
+    static void PrintNumbersRecursive(int current, int N)
+    {
+        if (current <= N)
+        {
+            Console.WriteLine(current);
+            PrintNumbersRecursive(current + 1, N);
+        }
+    }
+}
